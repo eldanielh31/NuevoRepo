92d18d7fac1bb86916ba83af2a4cdf5865512d57
electromilitary45 <derekl2045@hotmail.com>
2021-02-06 17:59:20 -0600

    cambiando titulo

--- prueba.py
+++ prueba.py
@@ -4,7 +4,7 @@ class Window:
     def __init__(self):
         window=Tk()
 
-        window.title('Pene')
+        window.title('hola')
         window.geometry("300x200+10+20")
         window.mainloop()
 
