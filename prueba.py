cf2ef9d14052d19588be35e6b48bc95d3a42f008
Daniel Brenes <danbg31@gmail.com>
2021-02-06 17:43:58 -0600

    primer commit

---
+++ prueba.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class Window:
+    def __init__(self):
+        window=Tk()
+
+        window.title('Pene')
+        window.geometry("300x200+10+20")
+        window.mainloop()
+
+window = Window()
